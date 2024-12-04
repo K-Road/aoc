a0344f86b81b6b5962a2bc57ae28d58de7aadd40
K-Road <game@chrodeprime.com>
2024-12-05 00:50:46 +1100

    setup d2p1

---
+++ cmd/2024/day2p1/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/K-Road/aoc/pkg/utils"
+)
+
+func main() {
+	//default for testing
+	//TODO
+	defaultFile := "cmd/2024/day2p1/test.input"
+
+	filepath := flag.String("e", defaultFile, "Path to the file to read")
+	flag.Parse()
+
+	file, err := os.Open(*filepath)
+	if err != nil {
+		fmt.Println("Error reading file", err)
+		return
+	}
+	defer file.Close()
+
+	var left []int
+	var right []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input := scanner.Text() //fmt.Println(scanner.Text())
+		strval := strings.Fields(input)
+
+		l, err := strconv.Atoi(strval[0])
+		if err != nil {
+			fmt.Println("Error converting", strval[0], "to int:", err)
+			return
+		}
+		r, err := strconv.Atoi(strval[1])
+		if err != nil {
+			fmt.Println("Error converting", strval[1], "to int:", err)
+			return
+		}
+
+		left = append(left, l)
+		right = append(right, r)
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading file %v\n", err)
+	}
+
+	// fmt.Println(left)
+	// fmt.Println(right)
+
+	//sort.Ints(left)
+	//sort.Ints(right)
+
+	counts := utils.CountOccurances(right)
+
+	// fmt.Println(left)
+	// fmt.Println(right)
+
+	sum := 0
+	for i := range left {
+		fmt.Println(counts[left[i]])
+		sum += left[i] * counts[left[i]]
+	}
+
+	score := 0
+	score = sum
+	fmt.Println(score)
+}
